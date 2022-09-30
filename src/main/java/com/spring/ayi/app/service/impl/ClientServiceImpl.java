ca21287b1a17b90733261a396a7f4c2eb11241d6
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2022-09-29 21:39:07 -0300

    feature(address): address added to client

--- src/main/java/com/spring/ayi/app/service/impl/ClientServiceImpl.java
+++ src/main/java/com/spring/ayi/app/service/impl/ClientServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.ayi.app.service.impl;
 
 import com.spring.ayi.app.dto.request.ClientRequest;
 import com.spring.ayi.app.dto.response.ClientResponse;
+import com.spring.ayi.app.entity.Address;
 import com.spring.ayi.app.entity.Client;
 import com.spring.ayi.app.entity.ClientDetail;
 import com.spring.ayi.app.mapper.IClientMapper;
@@ -9,6 +10,8 @@ import com.spring.ayi.app.repository.IClientRepository;
 import com.spring.ayi.app.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientServiceImpl implements IClientService {
@@ -27,8 +30,17 @@ public class ClientServiceImpl implements IClientService {
     public ClientResponse createClient(ClientRequest request) {
         Client client = clientMapper.convertDtoToEntity(request);
         ClientDetail clientDetail = client.getClientDetail();
+        List<Address> address = client.getAddresses();
+
+        if ( address != null && address.size() > 0) {
+            Client finalClient = client;
+            address.forEach(addr -> {
+                addr.setClient(finalClient);
+            });
+        }
 
         clientDetail.setClient(client);
+        client.setAddresses(address);
         client = clientRepository.save(client);
 
         return clientMapper.convertEntityToDto(client);
